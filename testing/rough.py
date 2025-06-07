7578b687060d228deff1f0addb4e2ac4956b1cf0
Ritesh <ritesh.sivanathan@gmail.com>
2025-06-07 16:30:07 -0400

    update rough.py

--- testing/rough.py
+++ testing/rough.py
@@ -1,7 +1,9 @@
-from ..mathematics.linear_algebra.matrices import Matrix
+from toolbox.mathematics.linear_algebra.matrices import Matrix
+from toolbox.mathematics.linear_algebra.MatOPS import MatOPS
+import sympy as sp
 
-A = Matrix(2,2, mode="fill",fill=5)
-print(A.matrix)
-print(A.show())
-print(A.det())
-print(repr(A))
+M = [[2, 0, 1, 3], [1, -1, 2, 4], [3, 2, 0, 1], [5, 1, 3, 2]]
+A = Matrix(man=M)
+print(A.inverse().matrix)
+print(MatOPS.inverse(A))
+print(sp.Matrix(M).inverse_ADJ())
