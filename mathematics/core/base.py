b3b4656017efd2579bc47c0092707ced4bd083b4
Ritesh Sivanathan <ritesh.sivanathan@gmail.com>
2025-07-31 23:08:32 -0400

    attempting to parse expressions

--- mathematics/core/base.py
+++ mathematics/core/base.py
@@ -4,7 +4,7 @@ class Constant:
   def __init__(self, value):
     
     self.value = value
-
+    self.traverse = False
     
 class Variable:
   
@@ -12,3 +12,36 @@ class Variable:
     
     self.value = value
     self.symbol = symbol
+    self.traverse = False
+    
+class Expression:
+  
+  def __init__(self, expression):
+    
+    self.expression = expression
+    self.traverse = True
+  
+  def evaluate(self):
+    
+    # Incomplete...
+    
+    first_part = self.expression.show()
+    
+    def dig(part):
+      
+      results = []
+      results.append(part)
+      
+      if part.traverse:
+        
+        parts = part.show()
+        
+        for part in parts:
+          res = dig(part)
+          results.append(res)
+                
+      return results
+    
+    print(dig(self.expression))
+    
+    return 0
