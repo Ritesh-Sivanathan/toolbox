a350c1143ef7cf758a1deeb5dbb67d44bedab0b2
Ritesh Sivanathan <ritesh.sivanathan@gmail.com>
2025-07-31 22:08:34 -0400

    added all basic operations

---
+++ mathematics/core/operators.py
@@ -0,0 +1,60 @@
+from .base import Constant, Variable
+
+class Add:
+  
+  def __init__(self, val_1, val_2):
+    
+    self.val_1:Constant = val_1
+    self.val_2:Constant = val_2
+  
+  def evaluate(self):
+    
+    if (type(self.val_1) == Variable and not self.val_1.value) or (type(self.val_2) == Variable and not self.val_2.value):
+
+      return f"{self.val_1.symbol if type(self.val_1) == Variable else self.val_1.value}+{self.val_2.symbol if type(self.val_2) == Variable else self.val_2.value}"
+ 
+    return self.val_1.value + self.val_2.value
+  
+class Subtract:
+  
+  def __init__(self, val_1, val_2):
+    
+    self.val_1:Constant = val_1
+    self.val_2:Constant = val_2
+  
+  def evaluate(self):
+    
+    return self.val_1.value - self.val_2.value
+
+class Multiply:
+  
+  def __init__(self, val_1, val_2):
+    
+    self.val_1:Constant = val_1
+    self.val_2:Constant = val_2
+  
+  def evaluate(self):
+    
+    return self.val_1.value * self.val_2.value
+  
+class Divide:
+  
+  def __init__(self, val_1, val_2):
+    
+    self.val_1:Constant = val_1
+    self.val_2:Constant = val_2
+  
+  def evaluate(self):
+    
+    return self.val_1.value / self.val_2.value
+
+class Power:
+  
+  def __init__(self, base, exp):
+    
+    self.base:Constant = base
+    self.exp:Constant = exp
+  
+  def evaluate(self):
+    
+    return self.base.value ** self.exp.value
