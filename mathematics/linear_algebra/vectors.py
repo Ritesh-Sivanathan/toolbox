391aee66c2af8294c689f1c05e5b5e427077230a
Ritesh <ritesh.sivanathan@gmail.com>
2025-10-24 15:20:59 -0400

    vector multiplication, cross product, dot product

--- mathematics/linear_algebra/vectors.py
+++ mathematics/linear_algebra/vectors.py
@@ -1,4 +1,4 @@
-
+from ..linear_algebra.matrices import Matrix
 
 class Vector:
 
@@ -14,7 +14,7 @@ class Vector:
         
         vector_sum = [0, 0, 0]
 
-        vector = self.__preprocess_(vector)
+        vector = self.__preprocess__(vector, same_size=True)
 
         for index in range(len(self.vector)):
             vector_sum[index] = self.vector[index] + vector.vector[index]
@@ -25,30 +25,101 @@ class Vector:
         
         vector_diff = [0, 0, 0]
 
-        vector = self.__preprocess_(vector)
+        vector = self.__preprocess__(vector, same_size=True)
 
         for index in range(len(self.vector)):
             vector_diff[index] = self.vector[index] - vector.vector[index]
         
         return Vector(vector_diff)
     
-    def __mul__(self, vector):
+    def __mul__(self, multiple):
 
         """
         
-        Dot product of two vectors
+        Dot product of two vectors (if )
         
         """
 
-        pass
+        if isinstance(multiple, (Vector, list)):
 
-    @staticmethod
-    def __preprocess_(vector):
+            vector = multiple
+            
+            vector = self.__preprocess__(vector, same_size=True)
 
-        if isinstance(vector, list[int]):
+            dot_product = 0
+
+            for index in range(len(vector.vector)):
+                dot_product += self.vector[index] * vector.vector[index]
+
+            return dot_product
+    
+    def n_dimensional_vector(self, n:int):
+
+        """
+        
+        Returns an n-dimensional vector filled with zeroes
+            [will add more options for fill values later]
+
+        """
+
+        if not isinstance(n, int):
+            return TypeError("n must be a valid integer")
+
+        vector = [0 for _ in range(n)]
+
+        return vector
+    
+    def cross_product(self, vector):
+        
+        """
+        
+        Cross product of two vectors in 3-space
+
+        """
+
+        vector = self.__preprocess__(self, vector)
+        
+        dimensions = len(vector.vector)
+
+        i = [1,0,0]
+        j = [0,1,0]
+        k = [0,0,1]
+
+        unit_vectors = [i,j,k]
+
+        dot_product = Vector([0,0,0])
+
+        for row in range(1, dimensions):
+            
+            new_matrix = [self.vector, vector.vector]
+            
+            for element in range(1, dimensions):
+
+                del new_matrix[row][element]
+                matrix = new_matrix
+                det, _ = Matrix(man=matrix).gaussian_elimination()
+                
+                dot_product += det * unit_vectors[element]
+
+        print(dot_product)
+        
+    def __preprocess__(self, vector, same_size=False):
+
+        if isinstance(vector, list) and all(isinstance(element, int) for element in vector):
 
             vector = Vector(vector)
+
+            if same_size and len(vector.vector) != len(self.vector):
+                print(len(vector.vector), len(self.vector))
+                raise ValueError("Vectors must be of the same size to perform this operation")
+            
+            return vector
         
-        if not isinstance(vector, Vector):
+        if not isinstance(vector, (Vector, list)):
 
             raise TypeError("Please provide a valid vector")
+        
+        if same_size and len(vector.vector) != len(self.vector):
+                raise ValueError("Vectors must be of the same size to perform this operation")
+
+        return vector
