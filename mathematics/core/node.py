8f3085d94056f4d0d9adb109def81655069826c3
Ritesh Sivanathan <ritesh.sivanathan@gmail.com>
2026-04-23 12:14:58 -0400

    slightly improved AST

--- mathematics/core/node.py
+++ mathematics/core/node.py
@@ -1,11 +1,80 @@
-class Node:
 
-    def __init__(self, left=None, right=None):
-        self.left = left
-        self.right = right
+class Add:
 
-    def evaluate(self):
+    def __init__(self,left,right):
+        self.left=left
+        self.right=right
+
+    def eval(self):
+
+        l = self.left.eval()
+        r = self.right.eval()
+
+        return l + r
+
+class Subtract:
+
+    def __init__(self,left,right):
+        self.left=left
+        self.right=right
+
+    def eval(self):
+
+        l = self.left.eval()
+        r = self.right.eval()
+
+        return l - r
+
+
+class Multiply:
+
+    def __init__(self,left,right):
+        self.left=left
+        self.right=right
+
+    def eval(self):
+
+        l = self.left.eval()
+        r = self.right.eval()
+
+        return l * r
+
+class Divide:
+
+    def __init__(self,left,right):
+        self.left=left
+        self.right=right
+
+    def eval(self):
+
+        l = self.left.eval()
+        r = self.right.eval()
+
+        return l / r
+
+
+class Constant:
+
+    def __init__(self,value):
+        self.value = value
+
+    def __add__(self, other):
+        return Add(self, other)
+
+    def __sub__(self,other):
+        return Subtract(self,other)
+
+    def __mul__(self,other):
+
+        return Multiply(self,other)
+
+    def __truediv__(self,other):
+
+        return Divide(self,other)
+
+    def eval(self):
         
-        if not self.left or not self.right:
-            return self
-       
+        if isinstance(self.value, (Add,Multiply)):
+            return self.value.eval()
+
+        return self.value
---
+++ mathematics/core/test.py
@@ -0,0 +1,3 @@
+from node import Add, Constant
+
+expr = Constant(1) + Constant(Constant(3)*Constant(4))
