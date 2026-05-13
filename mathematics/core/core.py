8283ce9f6e0297a26c9fd9ad9d361b9b85e2d592
Ritesh Sivanathan <ritesh.sivanathan@gmail.com>
2026-05-13 13:52:42 -0400

    remove eager evals

--- mathematics/core/core.py
+++ mathematics/core/core.py
@@ -1,5 +1,24 @@
 class BinaryOp:
 
+    """
+
+    Binary Operation. All binary operations inherit methods and properties from this class.
+
+    Properties/Attributes:
+
+        `self.left` (`Node`)
+        `self.right` (`Node`)
+
+    Ensures both the right and left nodes are actually nodes.
+
+    Currently implemented methods for BinaryOps:
+
+    - Add
+    - Multiply
+    - Equivalence
+
+    """
+
     def __init__(self,left,right):
         self.left=ensure_node(left)
         self.right=ensure_node(right)
@@ -11,42 +30,54 @@ class BinaryOp:
         return self.__add__(other)
 
     def __mul__(self,other):
-        return Multiply(other)
+        return Multiply(self,other)
 
     def __rmul__(self,other):
         return self.__mul__(other)
 
     def __eq__(self,other):
 
-        return (self.left.eval() == other.left.eval(), self.right.eval() == other.right.eval())
+        return (self.left == other.left, self.right == other.right)
 
 
 class Add(BinaryOp):
+
+    """
+
+    Add the left and right nodes.
+
+    Child of BinaryOp class.
+
+    """
 
     def eval(self):
 
         l = self.left.eval()
         r = self.right.eval()
 
-        print(type(l), type(r))
-
         return l + r
 
     def __str__(self):
         
-        return f"{self.left.eval()} + {self.right.eval()}"
+        return f"({self.left} + {self.right})"
 
     def __add__(self,other):
 
         other = ensure_node(other)
 
-        return Add(self.eval(),other.eval())
+        return Add(self,other)
 
     def __radd__(self,other):
 
         other = ensure_node(other)
 
-        return Add(self.eval(),other.eval())
+        return Add(self,other)
+
+    def __mul__(self,other):
+        return Multiply(self, other)
+
+    def __rmul__(self,other):
+        return self.__mul__(other)
 
 class Multiply(BinaryOp):
 
@@ -100,15 +131,15 @@ class Exponent(BinaryOp): # has many issues - no simplification for addition or 
 
     def __str__(self):
 
-        l = self.left.eval()
-        r = self.right.eval()
+        l = self.left
+        r = self.right
 
         return f"{l} ** {r}"
 
     def __mul__(self,other):
 
         if (isinstance(other,Constant)):
-            return Multiply(other,self.eval())
+            return Multiply(other,self)
 
         if (isinstance(other, Exponent)): # for now, the implementation will just ignore nested exponents (like if other.right is Multiply(Add(Exponent('x'),Exponent('x')) or something
 
@@ -131,6 +162,9 @@ class Constant:
 
     def __init__(self,value):
         self.value = value
+
+    def __str__(self):
+        return str(self.value)
 
     def __add__(self, other):
         return Add(self, other)
--- mathematics/core/test.py
+++ mathematics/core/test.py
@@ -1,11 +1,13 @@
 from core import *
 
-x = Variable('x')
-y = Variable('y')
+# x = Variable('x')
+# y = Variable('y')
 
 # expr = (x**2) * (y**2)
 # expr = 3 * x * 3
 # expr = 3*x + 2*x
-expr = x**3 + x**3
+expr = (Constant(1) + Constant(3)) + Constant(2)
+expr = Constant(3) * Constant(2) * Constant(4)
 
+print(expr)
 print(expr.eval())
