aff058f2d7c74204513f0df40f2b1bc5f2ac468b
Ritesh Sivanathan <ritesh.sivanathan@gmail.com>
2026-05-26 12:55:54 -0400

    add testcases

--- src/toolbox/mathematics/core/tests.py
+++ src/toolbox/mathematics/core/tests.py
@@ -22,6 +22,45 @@ class ConstantBinaryOperations(unittest.TestCase):
         expr = Constant(1) + self.x
         self.assertEqual(expr.eval(), Add(Constant(1), Variable('x')))
 
+    def test_BinOpsMultipleNestedConstants(self):
+        expr = (Constant(1) + (Constant(3)+Constant(2))) + (Constant(2)*(Constant(3)*Constant(2)))
+        self.assertEqual(expr.eval(),Constant(18))
+
+class VariableBinOps(unittest.TestCase):
+
+    a = Variable('a')
+    b = Variable('b')
+    c = Variable('c')
+    x = Variable('x')
+    y = Variable('y')
+    z = Variable('z')
+
+    def test_SimpleVarAdd(self):
+        expr = self.a + self.b
+        self.assertEqual(expr.eval(),Add(self.a,self.b))
+
+    def test_SimpleVarMul(self):
+        expr = self.a * self.b
+        self.assertEqual(expr.eval(),Multiply(self.a,self.b))
+
+    def test_SimplifyVarMul(self):
+        expr = self.a * self.a
+        self.assertEqual(expr.eval(),Exponent(self.a, Constant(2)))
+
+''' Expand is still in development - uncomment after done
+
+class TestExpand(unittest.TestCase):
+
+    e1 = (x + Constant(3)) * (x + Constant(2))
+    e2 = (x) * (x + Constant(2))
+    e3 = (x + Constant(3)) * x
+    e4 = (x + Constant(2)) * (x*y)
+
+    with self.subTest():
+        self.assertEqual(self.e1.expand(), ...)
+
+'''
+
 class ConstantUnaryOperations(unittest.TestCase):
 
     def test_ConstantPowConstant(self):
